f57ff620d6e5eea789a80c1ba3cb1d6a873f3101
Emilie Z <emilieyzh@gmail.com>
2022-05-09 03:01:18 -0400

    project ready for deployment

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__': # we are only executing this when we run the website
-    app.run(debug=True) # this starts up a web server
+    app.run(debug=True, host="0.0.0.0", port=5000) # this starts up a web server
     # debug = True will refresh the web server whenever you make a change
